a94c1c6318c6bfcebebe771c6b3347d7952b7a43
ZS1994 <1217360619@qq.com>
2020-04-23 20:25:27 +0800

    增加discovery，相当于提供“关于我们”的功能，可以获取到服务列表
    Signed-off-by: ZS1994 <1217360619@qq.com>

--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -6,9 +6,12 @@ import com.atguigu.springcloud.service.PaymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by 张顺 on 2020/4/22.
@@ -20,6 +23,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
@@ -43,5 +49,19 @@ public class PaymentController {
         }else{
             return new CommonResult<Payment>(444,"没有对应记录，查询ID",null);
         }
+    }
+
+    @GetMapping("/payment/discovery/info")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+
+        for (String ele : services){
+            log.info("******ele*********",ele);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances){
+            log.info(instance.getServiceId(),instance.getHost(),instance.getPort(),instance.getUri());
+        }
+        return discoveryClient;
     }
 }
--- cloud2020/cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
+++ cloud2020/cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @Slf4j
 public class PaymentMain8002 {
 
