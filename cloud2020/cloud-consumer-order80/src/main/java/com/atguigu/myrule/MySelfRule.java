a8faa6124fb0468a44eb3e47acd41e25e6264330
ZS1994 <1217360619@qq.com>
2020-04-24 14:50:21 +0800

    增加Ribbon自定义负载均衡策略的配置，改为了随机策略
    Signed-off-by: ZS1994 <1217360619@qq.com>

---
+++ cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -0,0 +1,21 @@
+package com.atguigu.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 张顺 on 2020/4/24.
+ */
+@Configuration
+public class MySelfRule {
+
+
+    @Bean
+    public IRule myRule(){
+        //定义为随机
+        return new RandomRule();
+    }
+
+}
--- cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -1,14 +1,20 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * Created by 张顺 on 2020/4/23.
+ *
+ *RibbonClient 的 name 是访问支付微服务名称,
+ * 这个注解的意思是不要使用默认出厂的轮训，而是使用我们自定义的随机
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
 
     public static void main(String[] args){
